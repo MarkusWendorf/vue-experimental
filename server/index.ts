7e85bc7e033a5f2378eb4010dfd225d1791cad03
Markus Wendorf <markuswendorf@web.de>
2019-02-26 20:37:52 +0100

    allow all origins

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import axios from "axios";
 import {NextFunction, Request, Response} from "express";
 
-const {API_KEY, allowedOrigins} = require("./config.json");
+const {API_KEY} = require("./config.json");
 const fontsApi = `https://www.googleapis.com/webfonts/v1/webfonts?sort=alpha&key=${API_KEY}`;
 const app = express();
 
@@ -10,28 +10,14 @@ const app = express();
 app.use((req: Request, res: Response, next: NextFunction) => {
 
     const origin = req.header("origin") || "NONE";
-
-    if (allowedOrigins.some((allowed: string) => origin === allowed)) {
-        res.setHeader("Access-Control-Allow-Origin", origin);
-        next();
-    } else {
-        res.sendStatus(403);
-    }
+    res.setHeader("Access-Control-Allow-Origin", origin);
 });
 
 
 // Route: Serve font list
-let cached: any[];
 app.get("/fonts", async (req: Request, res: Response) => {
 
     console.log("GET /fonts");
-
-    if (cached) {
-        console.log("from Cache");
-        res.contentType("application/json");
-        res.send(cached);
-        return;
-    }
 
     const response = await axios.get(fontsApi);
 
@@ -40,7 +26,6 @@ app.get("/fonts", async (req: Request, res: Response) => {
 
         // only fonts that have a regular weight/type
         const fonts = json.items.filter((font: any) => !!font.files["regular"]);
-        cached = fonts;
 
         res.contentType("application/json");
         res.send(fonts);
